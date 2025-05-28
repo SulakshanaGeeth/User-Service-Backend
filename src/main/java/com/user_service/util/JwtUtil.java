7594b2a17722b598d818fb296b48be239cd35e7d
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-05-28 20:55:34 +0530

    create JWT util class
    Signed-off-by: SulakshanaGeeth <sulakshanageeth@gmail.com>

---
+++ src/main/java/com/user_service/util/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.user_service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "my_secret_key";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token)
+                .getBody().getSubject();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername());
+    }
+}
