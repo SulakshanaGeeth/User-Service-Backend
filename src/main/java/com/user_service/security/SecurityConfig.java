f313da2941aa9992931bf3d504651a2faf37dc16
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-05-31 21:37:41 +0530

    permit user registration end-point
    Signed-off-by: SulakshanaGeeth <sulakshanageeth@gmail.com>

--- src/main/java/com/user_service/security/SecurityConfig.java
+++ src/main/java/com/user_service/security/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/authenticate").permitAll()
+                .requestMatchers("/api/v1/public/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
