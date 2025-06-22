17fc243d315baba2018bebc4525a41720fa58c42
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-06-22 21:24:32 +0530

    create role repository and PermissionRepository
    Signed-off-by: SulakshanaGeeth <sulakshanageeth@gmail.com>

---
+++ src/main/java/com/user_service/repository/PermissionRepository.java
@@ -0,0 +1,10 @@
+package com.user_service.repository;
+
+import com.user_service.model.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Optional<Permission> findByName(String name);
+}
