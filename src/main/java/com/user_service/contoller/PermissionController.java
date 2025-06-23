cff1b04582f2a18dd4116abdb962b4aba82cbc9c
SulakshanaGeeth <sulakshanageeth@gmail.com>
2025-06-23 23:33:38 +0530

    minor change on PermissionController and PermissionService
    Signed-off-by: SulakshanaGeeth <sulakshanageeth@gmail.com>

--- src/main/java/com/user_service/contoller/PermissionController.java
+++ src/main/java/com/user_service/contoller/PermissionController.java
@@ -3,11 +3,11 @@ package com.user_service.contoller;
 import com.user_service.model.Permission;
 import com.user_service.service.PermissionService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/permissions")
@@ -16,7 +16,13 @@ public class PermissionController {
     private PermissionService permissionService;
 
     @PostMapping
-    public ResponseEntity<?> create( @RequestBody Permission permission) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(permissionService.createPermission(permission));
+    public ResponseEntity<Permission> create( @RequestBody Permission permission) {
+        Permission createdPermission = permissionService.createPermission(permission);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPermission);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Permission>> findAll(){
+        return ResponseEntity.ok(permissionService.getPermissions());
     }
 }
--- src/main/java/com/user_service/service/PermissionService.java
+++ src/main/java/com/user_service/service/PermissionService.java
@@ -5,6 +5,7 @@ import com.user_service.repository.PermissionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
 
 @Service
@@ -20,5 +21,9 @@ public class PermissionService {
     @Transactional                // override: write transaction
     public Permission createPermission(Permission permission){
         return permissionRepository.save(permission);
+    }
+
+    public List<Permission> getPermissions(){
+        return permissionRepository.findAll();
     }
 }
